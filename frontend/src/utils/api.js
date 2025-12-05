0d858e882205a47f72d2983d8eab5cdfc78aedae
xampp-developer <xamppdeveloper@gmail.com>
2025-12-06 01:04:06 +0530

    Fix API URL for Vercel

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "https://clever-intuition-production.up.railway.app/api",
+    baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json'
   }
